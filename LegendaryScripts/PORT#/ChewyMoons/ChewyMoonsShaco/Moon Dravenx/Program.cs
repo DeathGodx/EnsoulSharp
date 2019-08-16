f901665d3aaad6f1001ec2de31b09faa7b1144bc
Death <52470824+DeathGodx@users.noreply.github.com>
2019-08-16 17:21:03 +0300

    Add files via upload

---
+++ LegendaryScripts/PORT#/ChewyMoons/ChewyMoonsShaco/Moon Dravenx/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace MoonDraven
+{
+    using System;
+
+    using System;
+    using System.Linq;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using EnsoulSharp.SDK;
+    using EnsoulSharp.SDK.MenuUI;
+    using EnsoulSharp.SDK.MenuUI.Values;
+    using EnsoulSharp.SDK.Prediction;
+    using EnsoulSharp.SDK.Utility;
+    using EnsoulSharp;
+    using SharpDX;
+    using static EnsoulSharp.SDK.Items;
+    using SharpDX.Direct3D9;
+    using static EnsoulSharp.SDK.Interrupter;
+
+    internal class Program
+    {
+        #region Methods
+
+        private static void GameOnOnGameLoad()
+        {
+            if (ObjectManager.Player.Name == "Draven")
+            {
+                new MoonDraven().Load();
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            GameEvent.OnGameLoad += GameOnOnGameLoad;
+        }
+
+        #endregion
+    }
+}
